e26efba58748900ba76c1e2c6cdc8092c567594c
GibsonPrime <mcarrie@gmail.com>
2018-03-03 11:45:54 +0000

    Fixed MainForm call after renaming from form1

--- ProcessHelper/ProcessHelper/My Project/Application.Designer.vb
+++ ProcessHelper/ProcessHelper/My Project/Application.Designer.vb
@@ -32,7 +32,7 @@ Namespace My
 
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()> _
         Protected Overrides Sub OnCreateMainForm()
-            Me.MainForm = Global.ProcessHelper.Form1
+            Me.MainForm = Global.ProcessHelper.MainForm
         End Sub
     End Class
 End Namespace
