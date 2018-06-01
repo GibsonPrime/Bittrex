4b8e0efe884ba7d61a09c0829bfd8a59c9865e14
GibsonPrime <mcarrie@gmail.com>
2018-06-01 07:24:50 +0100

    Initial commit of windows update killer

---
+++ NoWinUpdate/NoWinUpdate/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System.Threading;
+using System.Diagnostics;
+using System.Windows.Forms;
+
+namespace NoWinUpdate
+{
+    public partial class Form1 : Form
+    {
+        private static string PROCESS_NAME = "Windows10UpgraderApp";
+
+        public Form1()
+        {
+            InitializeComponent();
+            Visible = false;
+            ShowInTaskbar = false;
+            run();      
+        }
+    
+        private void run()
+        {
+            while (true)
+            {
+                foreach (Process p in System.Diagnostics.Process.GetProcesses())
+                    if (p.ProcessName.Equals(Form1.PROCESS_NAME))
+                        p.Kill();
+                Thread.Sleep(1000);
+            }
+        }
+    }
+}
